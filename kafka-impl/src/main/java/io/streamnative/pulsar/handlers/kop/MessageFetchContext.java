streamnative/kop

[Transaction] Support producer state manager (#912)
Master issues: #915

This PR is a copy from #796, rebase from the master branch. And fix all tests. 

### Motivation
KoP doesn't support duplicate sequence number checks, it's used for the idempotent producers.

Each topic should have a `ProducerStateManager`, the `ProducerStateManager` will ensure idempotent when a producer has produced.

### Modifications
**Note**: Current implement just in-memory support, it should be persisted to some storage. And we only support `MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION = 1` when using idempotent producer.

1. Support in memory producer state manager
2. Support idempotent producer
3. Add new units test `ProducerStateManagerTest`

**Addition change after #796**

1. Use exists `ReplicaManager#appendRecords` to write the transaction markers.
2. Refactor make the code cleaner.
3. Remove unused aborted Index logic in transaction coordinator.

### TODO
* Support Producer ID Expiration
* Support snapshot
* Support `MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION = 5` when using idempotent producer(Is hard to support it, since KoP uses async message publishing, is hard to add lock in this method).

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/MessageFetchContext.java
@@ -24,6 +24,7 @@ import io.streamnative.pulsar.handlers.kop.exceptions.MetadataCorruptedException
 import io.streamnative.pulsar.handlers.kop.format.DecodeResult;
 import io.streamnative.pulsar.handlers.kop.security.auth.Resource;
 import io.streamnative.pulsar.handlers.kop.security.auth.ResourceType;
+import io.streamnative.pulsar.handlers.kop.storage.PartitionLog;
 import io.streamnative.pulsar.handlers.kop.utils.GroupIdUtils;
 import io.streamnative.pulsar.handlers.kop.utils.KopTopic;
 import io.streamnative.pulsar.handlers.kop.utils.MessageMetadataUtils;
@@ -67,7 +68,6 @@ import org.apache.kafka.common.requests.FetchResponse.PartitionData;
 import org.apache.kafka.common.requests.IsolationLevel;
 import org.apache.kafka.common.requests.RequestHeader;
 import org.apache.kafka.common.requests.ResponseCallbackWrapper;
-import org.apache.pulsar.common.naming.TopicName;
 import org.apache.pulsar.metadata.api.GetResult;
 
 /**
@@ -385,7 +385,6 @@ public final class MessageFetchContext {
                                                     entries,
                                                     topicPartition,
                                                     partitionData,
-                                                    fullTopicName,
                                                     tcm,
                                                     cursor,
                                                     cursorOffset,
@@ -402,7 +401,6 @@ public final class MessageFetchContext {
     private void handleEntries(final List<Entry> entries,
                                final TopicPartition topicPartition,
                                final FetchRequest.PartitionData partitionData,
-                               final String fullTopicName,
                                final KafkaTopicConsumerManager tcm,
                                final ManagedCursor cursor,
                                final AtomicLong cursorOffset,
@@ -410,9 +408,10 @@ public final class MessageFetchContext {
         final long highWatermark = MessageMetadataUtils.getHighWatermark(cursor.getManagedLedger());
         // Add new offset back to TCM after entries are read successfully
         tcm.add(cursorOffset.get(), Pair.of(cursor, cursorOffset.get()));
-
+        PartitionLog partitionLog =
+                requestHandler.getReplicaManager().getPartitionLog(topicPartition, namespacePrefix);
         final long lso = (readCommitted
-                ? tc.getLastStableOffset(TopicName.get(fullTopicName), highWatermark) : highWatermark);
+                ? partitionLog.firstUndecidedOffset().orElse(highWatermark) : highWatermark);
         List<Entry> committedEntries = entries;
         if (readCommitted) {
             committedEntries = getCommittedEntries(entries, lso);
@@ -479,8 +478,12 @@ public final class MessageFetchContext {
                     entries.size(),
                     groupName,
                     statsLogger);
-            final List<FetchResponse.AbortedTransaction> abortedTransactions =
-                    (readCommitted ? tc.getAbortedIndexList(partitionData.fetchOffset) : null);
+            List<FetchResponse.AbortedTransaction> abortedTransactions;
+            if (requestHandler.getKafkaConfig().isKafkaTransactionCoordinatorEnabled() && readCommitted && tc != null) {
+                abortedTransactions = partitionLog.getAbortedIndexList(partitionData.fetchOffset);
+            } else {
+                abortedTransactions = null;
+            }
             responseData.put(topicPartition, new PartitionData<>(
                     Errors.NONE,
                     highWatermark,

tests/src/test/java/io/streamnative/pulsar/handlers/kop/IdempotentProducerTest.java
@@ -1,0 +1,95 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamnative.pulsar.handlers.kop;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.pulsar.client.admin.PulsarAdminException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Transaction test.
+ */
+@Slf4j
+public class IdempotentProducerTest extends KopProtocolHandlerTestBase {
+
+    @BeforeClass
+    @Override
+    protected void setup() throws Exception {
+        this.conf.setKafkaTransactionCoordinatorEnabled(true);
+        super.internalSetup();
+        log.info("success internal setup");
+    }
+
+    @AfterClass
+    @Override
+    protected void cleanup() throws Exception {
+        super.internalCleanup();
+    }
+
+    @DataProvider(name = "produceConfigProvider")
+    protected static Object[][] produceConfigProvider() {
+        // isBatch
+        return new Object[][]{
+                {true},
+                {false}
+        };
+    }
+
+    @Test
+    public void testIdempotentProducer() throws PulsarAdminException {
+        String topic = "testIdempotentProducer";
+        admin.topics().createPartitionedTopic(topic, 1);
+        int maxMessageNum = 1000;
+        Properties producerProperties = newKafkaProducerProperties();
+        producerProperties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        producerProperties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1);
+
+        @Cleanup
+        KafkaProducer<String, String> producer = new KafkaProducer<>(producerProperties);
+
+        for (int i = 0; i < maxMessageNum; i++) {
+            producer.send(new ProducerRecord<>(topic, "test" + i));
+        }
+        producer.flush();
+
+        @Cleanup
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(newKafkaConsumerProperties());
+        consumer.subscribe(Collections.singleton(topic));
+        int i = 0;
+        while (i < maxMessageNum) {
+            ConsumerRecords<String, String> messages = consumer.poll(Duration.ofSeconds(2));
+            for (ConsumerRecord<String, String> message : messages) {
+                assertEquals("test" + i, message.value());
+                i++;
+            }
+        }
+        assertEquals(maxMessageNum, i);
+    }
+
+}

datastax/starlight-for-kafka

This PR is a copy from #796, rebase from the master branch. And fix all tests.
(cherry picked from commit fee4cad4addf0edc0754f8c8e43de62df4ef9187)

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/MessageFetchContext.java
@@ -24,6 +24,7 @@ import io.streamnative.pulsar.handlers.kop.exceptions.MetadataCorruptedException
 import io.streamnative.pulsar.handlers.kop.format.DecodeResult;
 import io.streamnative.pulsar.handlers.kop.security.auth.Resource;
 import io.streamnative.pulsar.handlers.kop.security.auth.ResourceType;
+import io.streamnative.pulsar.handlers.kop.storage.PartitionLog;
 import io.streamnative.pulsar.handlers.kop.utils.GroupIdUtils;
 import io.streamnative.pulsar.handlers.kop.utils.KopTopic;
 import io.streamnative.pulsar.handlers.kop.utils.MessageMetadataUtils;
@@ -427,7 +428,6 @@ public final class MessageFetchContext {
                                                     entries,
                                                     topicPartition,
                                                     partitionData,
-                                                    fullTopicName,
                                                     tcm,
                                                     cursor,
                                                     cursorOffset,
@@ -444,7 +444,6 @@ public final class MessageFetchContext {
     private void handleEntries(final List<Entry> entries,
                                final TopicPartition topicPartition,
                                final FetchRequest.PartitionData partitionData,
-                               final String fullTopicName,
                                final KafkaTopicConsumerManager tcm,
                                final ManagedCursor cursor,
                                final AtomicLong cursorOffset,
@@ -452,9 +451,10 @@ public final class MessageFetchContext {
         final long highWatermark = MessageMetadataUtils.getHighWatermark(cursor.getManagedLedger());
         // Add new offset back to TCM after entries are read successfully
         tcm.add(cursorOffset.get(), Pair.of(cursor, cursorOffset.get()));
-
+        PartitionLog partitionLog =
+                requestHandler.getReplicaManager().getPartitionLog(topicPartition, namespacePrefix);
         final long lso = (readCommitted
-                ? tc.getLastStableOffset(TopicName.get(fullTopicName), highWatermark) : highWatermark);
+                ? partitionLog.firstUndecidedOffset().orElse(highWatermark) : highWatermark);
         List<Entry> committedEntries = entries;
         if (readCommitted) {
             committedEntries = getCommittedEntries(entries, lso);
@@ -521,8 +521,12 @@ public final class MessageFetchContext {
                     entries.size(),
                     groupName,
                     statsLogger);
-            final List<FetchResponse.AbortedTransaction> abortedTransactions =
-                    (readCommitted ? tc.getAbortedIndexList(partitionData.fetchOffset) : null);
+            List<FetchResponse.AbortedTransaction> abortedTransactions;
+            if (requestHandler.getKafkaConfig().isTransactionCoordinatorEnabled() && readCommitted && tc != null) {
+                abortedTransactions = partitionLog.getAbortedIndexList(partitionData.fetchOffset);
+            } else {
+                abortedTransactions = null;
+            }
             responseData.put(topicPartition, new PartitionData<>(
                     Errors.NONE,
                     highWatermark,

tests/src/test/java/io/streamnative/pulsar/handlers/kop/IdempotentProducerTest.java
@@ -1,0 +1,95 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamnative.pulsar.handlers.kop;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.pulsar.client.admin.PulsarAdminException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Transaction test.
+ */
+@Slf4j
+public class IdempotentProducerTest extends KopProtocolHandlerTestBase {
+
+    @BeforeClass
+    @Override
+    protected void setup() throws Exception {
+        this.conf.setTransactionCoordinatorEnabled(true);
+        super.internalSetup();
+        log.info("success internal setup");
+    }
+
+    @AfterClass
+    @Override
+    protected void cleanup() throws Exception {
+        super.internalCleanup();
+    }
+
+    @DataProvider(name = "produceConfigProvider")
+    protected static Object[][] produceConfigProvider() {
+        // isBatch
+        return new Object[][]{
+                {true},
+                {false}
+        };
+    }
+
+    @Test
+    public void testIdempotentProducer() throws PulsarAdminException {
+        String topic = "testIdempotentProducer";
+        admin.topics().createPartitionedTopic(topic, 1);
+        int maxMessageNum = 1000;
+        Properties producerProperties = newKafkaProducerProperties();
+        producerProperties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        producerProperties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1);
+
+        @Cleanup
+        KafkaProducer<String, String> producer = new KafkaProducer<>(producerProperties);
+
+        for (int i = 0; i < maxMessageNum; i++) {
+            producer.send(new ProducerRecord<>(topic, "test" + i));
+        }
+        producer.flush();
+
+        @Cleanup
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(newKafkaConsumerProperties());
+        consumer.subscribe(Collections.singleton(topic));
+        int i = 0;
+        while (i < maxMessageNum) {
+            ConsumerRecords<String, String> messages = consumer.poll(Duration.ofSeconds(2));
+            for (ConsumerRecord<String, String> message : messages) {
+                assertEquals("test" + i, message.value());
+                i++;
+            }
+        }
+        assertEquals(maxMessageNum, i);
+    }
+
+}