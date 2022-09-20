b83a9437760ffee5e8d47d546b06205d05b1f903
sunhangda <lwclover@126.com>
2022-09-20 18:37:16 +0800

    [BUG#1500] Cannot submit offset correctly (#1501)
    Fixes #1500
    
    ### Motivation
    
    The baseOffset is not monotonically increasing, consumer cannot submit offset correctly.
    
    ### Modifications
    
    When the offset is submitted, the increasing judgment mode of baseOffset is changed
    
    ### Verifying this change
    
    This change is already covered by existing tests.
    
    1.passed GroupMetadataManagerTest
    2.passed GroupMetadataTest
    
    (cherry picked from commit 0e6fde0cbe6b09e042f5bca987047740dc168737)

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/coordinator/group/GroupMetadataManagerTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/coordinator/group/GroupMetadataManagerTest.java
@@ -61,6 +61,7 @@ import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.bookkeeper.common.util.MathUtils;
 import org.apache.bookkeeper.common.util.OrderedScheduler;
+import org.apache.bookkeeper.mledger.impl.PositionImpl;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.protocol.Errors;
 import org.apache.kafka.common.record.AbstractRecords;
@@ -766,14 +767,14 @@ public class GroupMetadataManagerTest extends KopProtocolHandlerTestBase {
         committedOffsetsFirstProducer.forEach((tp, offset) -> {
             assertEquals(Optional.of(offset), group.offset(tp, NAMESPACE_PREFIX).map(OffsetAndMetadata::offset));
             assertEquals(
-                Optional.of((long) firstProduceRecordOffset),
-                group.offsetWithRecordMetadata(tp).flatMap(CommitRecordMetadataAndOffset::appendedBatchOffset));
+                Optional.of(new PositionImpl(0, firstProduceRecordOffset)),
+                group.offsetWithRecordMetadata(tp).flatMap(CommitRecordMetadataAndOffset::appendedPosition));
         });
         committedOffsetsSecondProducer.forEach((tp, offset) -> {
             assertEquals(Optional.of(offset), group.offset(tp, NAMESPACE_PREFIX).map(OffsetAndMetadata::offset));
             assertEquals(
-                Optional.of((long) secondProduceRecordOffset),
-                group.offsetWithRecordMetadata(tp).flatMap(CommitRecordMetadataAndOffset::appendedBatchOffset));
+                Optional.of(new PositionImpl(0, secondProduceRecordOffset)),
+                group.offsetWithRecordMetadata(tp).flatMap(CommitRecordMetadataAndOffset::appendedPosition));
         });
 
     }
