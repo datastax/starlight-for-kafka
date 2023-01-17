9f3416e9ac27200ed8da0e5dd88fc33288b8865a
Enrico Olivelli <eolivelli@apache.org>
2023-01-17 08:57:50 +0100

    [oauthclient] Rename package name to com.datastax.oss.kafka.oauth (#47)

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/TransactionWithOAuthBearerAuthTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/TransactionWithOAuthBearerAuthTest.java
@@ -13,9 +13,9 @@
  */
 package io.streamnative.pulsar.handlers.kop;
 
+import com.datastax.oss.kafka.oauth.OauthLoginCallbackHandler;
+import com.datastax.oss.kafka.oauth.OauthValidatorCallbackHandler;
 import com.google.common.collect.Sets;
-import io.streamnative.pulsar.handlers.kop.security.oauth.OauthLoginCallbackHandler;
-import io.streamnative.pulsar.handlers.kop.security.oauth.OauthValidatorCallbackHandler;
 import java.net.URL;
 import java.util.Properties;
 import lombok.extern.slf4j.Slf4j;
