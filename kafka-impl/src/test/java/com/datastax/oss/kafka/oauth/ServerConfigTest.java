704a820c9a33ec8406d55c6a60dc768d10dc1d70
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-01-20 15:34:57 +0100

    checkstyle

--- kafka-impl/src/test/java/com/datastax/oss/kafka/oauth/ServerConfigTest.java
+++ kafka-impl/src/test/java/com/datastax/oss/kafka/oauth/ServerConfigTest.java
@@ -13,10 +13,9 @@
  */
 package com.datastax.oss.kafka.oauth;
 
+import io.streamnative.pulsar.handlers.kop.security.auth.ServerConfig;
 import java.util.HashMap;
 import java.util.Map;
-
-import io.streamnative.pulsar.handlers.kop.security.auth.ServerConfig;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
