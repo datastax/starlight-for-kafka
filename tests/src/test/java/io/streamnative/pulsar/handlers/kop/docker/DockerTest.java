3c77b6de708b0b7f0ba755eaf7152cda6e932bd7
mallasandeep <sandeep.malla78@gmail.com>
2025-04-29 19:51:47 +0530

    Updated confluent/kafka image version to use 7.8.2 as the latest(7.9.0) is not supported yet.

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/docker/DockerTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/docker/DockerTest.java
@@ -30,8 +30,8 @@ public class DockerTest {
 
     private static final String IMAGE_LUNASTREAMING40 = "datastax/lunastreaming:4.0_3.2";
     private static final String IMAGE_PULSAR40 = "apachepulsar/pulsar:4.0.4";
-    private static final String CONFLUENT_CLIENT = "confluentinc/cp-kafka:latest";
-    private static final String CONFLUENT_SCHEMAREGISTRY_CLIENT = "confluentinc/cp-schema-registry:latest";
+    private static final String CONFLUENT_CLIENT = "confluentinc/cp-kafka:7.8.2";
+    private static final String CONFLUENT_SCHEMAREGISTRY_CLIENT = "confluentinc/cp-schema-registry:7.8.2";
 
     @Test
     public void test() throws Exception {
