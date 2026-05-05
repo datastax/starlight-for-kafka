aad86ae26482ebccd9ccec00e34b5ccee8b13084
priyanshu-ctds <ext-priyanshu.pareta@datastax.com>
2026-05-05 16:10:16 +0530

    Removed unwanted spaces

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
@@ -3182,44 +3182,35 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                                                                        Map<TopicPartition, Errors> partitionErrors){
         Map<String, AddPartitionsToTxnResponseData.AddPartitionsToTxnPartitionResultCollection> resultMap =
             new HashMap<>();
-
         for (Map.Entry<TopicPartition, Errors> entry : partitionErrors.entrySet()) {
             TopicPartition tp = entry.getKey();
             Errors error = entry.getValue();
-
             AddPartitionsToTxnResponseData.AddPartitionsToTxnPartitionResult partitionResult =
                 new AddPartitionsToTxnResponseData.AddPartitionsToTxnPartitionResult()
                     .setPartitionIndex(tp.partition())
                     .setPartitionErrorCode(error.code());
-
             AddPartitionsToTxnResponseData.AddPartitionsToTxnPartitionResultCollection partitionCollection =
                 resultMap.getOrDefault(
                     tp.topic(),
                     new AddPartitionsToTxnResponseData.AddPartitionsToTxnPartitionResultCollection()
                 );
-
             partitionCollection.add(partitionResult);
             resultMap.put(tp.topic(), partitionCollection);
         }
-
         AddPartitionsToTxnResponseData.AddPartitionsToTxnTopicResultCollection topicCollection =
             new AddPartitionsToTxnResponseData.AddPartitionsToTxnTopicResultCollection();
-
         for (Map.Entry<String,
             AddPartitionsToTxnResponseData.AddPartitionsToTxnPartitionResultCollection> entry
             : resultMap.entrySet()) {
-
             topicCollection.add(
                 new AddPartitionsToTxnResponseData.AddPartitionsToTxnTopicResult()
                     .setName(entry.getKey())
                     .setResultsByPartition(entry.getValue())
             );
         }
-
         AddPartitionsToTxnResponseData data = new AddPartitionsToTxnResponseData()
             .setThrottleTimeMs(throttleTimeMs)
             .setResultsByTopicV3AndBelow(topicCollection);
-
         return new AddPartitionsToTxnResponse(data);
     }
 }
